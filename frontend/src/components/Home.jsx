eddf1f44f4cd5bc6f88a90c4c85c69ea1a0644ad
Bhargav Jagani <jaganibhargav30@gmail.com>
2024-07-31 10:24:00 +0530

    fetch quote

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-import Auth from './Auth'
+import React, { useEffect } from "react";
+import Auth from "./Auth";
+import axios from "axios";
 
 export default function Home() {
-    Auth()
-  return (
-    <div>Home</div>
-  )
+  Auth();
+
+  const API = "http://127.0.0.1:3005/api/quote/getquote";
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get(API, {
+            headers : {
+                "x-access-token" : localStorage.getItem('token')
+            }
+        })
+        console.log(response)
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchData()
+  }, []);
+
+  return <div>Home</div>;
 }
